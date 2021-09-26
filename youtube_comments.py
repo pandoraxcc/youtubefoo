429a13ada68cfc1a375fbb0fb7cfdb0f7c079ddf
pandoraxcc <60555311+pandoraxcc@users.noreply.github.com>
2021-09-26 11:01:06 -0800

    Add files via upload

--- youtube_comments.py
+++ youtube_comments.py
@@ -7,36 +7,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import NoSuchElementException
 
-print(os.getcwd())
+# Path to the execution file of webdriver
+current_path = os.getcwd()
+full_path = current_path + "\chromedriver.exe"
+
+
 class ParserYoutube:
 
-    def __init__(self, web_adress, comment_type = "neutral"):
+    def __init__(self, web_adress, full_path, comment_type = "neutral"):
         # sending the adress of the web page 
         self.web_adress = web_adress
         self.comment_type = comment_type
-        self.browser = webdriver.Chrome(executable_path=r'C:\Users\Dmitrii\Desktop\scraping\chromedriver.exe')
+        self.full_path = full_path
+        self.browser = webdriver.Chrome(executable_path = self.full_path)
 
-    def scan_page_for_comments(self):
-        no_comments = "Comments are turned off"
-        
-        try:
-            self.browser.get(self.web_adress)
-            comments = self.browser.find_element_by_css_selector("#guide").text
-            
-            if  no_comments in comments:
-                self.browser.quit()
-                return print("No comments on the page. Quitting")
-            
-            else:
-                print("There are comments on the page")
-                self.browser.quit()
-        
-        except Exception as e:
-            print(f"failed to get {self.web_adress} \n")
-            print(e)
-            
-
-        
+    
     def scrape_all_comments(self):
         pass
 
@@ -52,5 +37,8 @@ class ResultAnalyzer:
     def count_elements(self):
         pass
 
-test_1 = ParserYoutube("https://www.youtube.com/watch?v=6JhVo2zS8hU")
+# No comments
+#https://www.youtube.com/watch?v=6JhVo2zS8hU
+
+test_1 = ParserYoutube("https://www.youtube.com/watch?v=vyqSdJLVQgg", full_path)
 test_1.scan_page_for_comments()
