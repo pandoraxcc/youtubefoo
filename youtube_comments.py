cd828623f02be089e0e2ad9806dbf7f07bfc4e7b
pandoraxcc <60555311+pandoraxcc@users.noreply.github.com>
2021-10-19 12:37:45 -0800

    comments load fucntion

--- youtube_comments.py
+++ youtube_comments.py
@@ -28,3 +28,26 @@ class ParserYoutube:
         self.web_adress = web_adress
         self.full_path = full_path
         self.browser = webdriver.Chrome(executable_path = self.full_path)
+        
+        
+ def load_all_comments(self):
+
+        self.browser.get(self.web_adress)
+        self.browser.maximize_window()
+        time.sleep(3)
+        # We need to locate the comment sector in order to render it
+        target_section = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments')
+        self.browser.execute_script("arguments[0].scrollIntoView();", target_section)
+        time.sleep(4)
+        
+        # Check if the comments are off:
+        try:
+            is_off_path = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-message-renderer/yt-formatted-string[1]/span').text
+            for x in range(5):
+                print("\n")
+                print("\n")
+                print("Comments are off. closing the windows")
+            self.browser.quit()
+        except NoSuchElementException:
+            pass
+        
