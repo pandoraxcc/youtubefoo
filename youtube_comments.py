bb653c5042081fb3ede08f8eccdac31c739f511d
pandoraxcc <60555311+pandoraxcc@users.noreply.github.com>
2021-10-24 11:40:08 -0800

    Getting youtube comments

--- youtube_comments.py
+++ youtube_comments.py
@@ -30,44 +30,56 @@ class ParserYoutube:
         self.browser = webdriver.Chrome(executable_path = self.full_path)
         
         
- def load_all_comments(self):
+   def load_all_comments(self):
 
-        self.browser.get(self.web_adress)
-        self.browser.maximize_window()
-        time.sleep(3)
-        # We need to locate the comment sector in order to render it
-        target_section = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments')
-        self.browser.execute_script("arguments[0].scrollIntoView();", target_section)
-        time.sleep(4)
+          self.browser.get(self.web_adress)
+          self.browser.maximize_window()
+          time.sleep(3)
+          # We need to locate the comment sector in order to render it
+          target_section = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments')
+          self.browser.execute_script("arguments[0].scrollIntoView();", target_section)
+          time.sleep(4)
+
+          # Check if the comments are off:
+          try:
+              is_off_path = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-message-renderer/yt-formatted-string[1]/span').text
+              for x in range(5):
+                  print("\n")
+                  print("\n")
+                  print("Comments are off. closing the windows")
+              self.browser.quit()
+          except NoSuchElementException:
+              pass
         
-        # Check if the comments are off:
-        try:
-            is_off_path = self.browser.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-message-renderer/yt-formatted-string[1]/span').text
-            for x in range(5):
-                print("\n")
-                print("\n")
-                print("Comments are off. closing the windows")
-            self.browser.quit()
-        except NoSuchElementException:
-            pass
+  def scroll_down(self):
+          # Get scroll height.
+          max_height = self.browser.execute_script("return document.body.scrollHeight")
+
+          while True:
+
+              # Scroll down to the bottom.
+              self.browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+              max_height = self.browser.execute_script("return document.documentElement.scrollHeight")
+              # Wait to load the page.
+              time.sleep(2)
+
+              # Calculate new scroll height and compare with last scroll height.
+              new_height = self.browser.execute_script("return document.documentElement.scrollHeight")
+
+              if new_height == max_height:
+                  break
+
+              max_height = new_height
+
+    def get_all_comments(self):
         
-def scroll_down(self):
-        # Get scroll height.
-        max_height = self.browser.execute_script("return document.body.scrollHeight")
-
-        while True:
-
-            # Scroll down to the bottom.
-            self.browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-            max_height = self.browser.execute_script("return document.documentElement.scrollHeight")
-            # Wait to load the page.
-            time.sleep(2)
-
-            # Calculate new scroll height and compare with last scroll height.
-            new_height = self.browser.execute_script("return document.documentElement.scrollHeight")
-
-            if new_height == max_height:
-                break
-
-            max_height = new_height
-
+        comments_lst = []
+        #comments = self.browser.find_elements_by_xpath('//*[@id="content-text"]')
+        comments = self.browser.find_elements_by_css_selector("#content-text")
+        for comment in comments:
+            comments_lst.append(comment.text)
+            print(comment.text)
+            #print("\n")
+        #print(len(comments))
+        
+        return comments_lst
